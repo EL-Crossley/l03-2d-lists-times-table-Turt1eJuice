b88c8b9286f8ab3a643898942bedf1c75cada531
Turt1eJuice <turt1ejuice@outlook.com>
2024-10-24 13:57:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-# Put your function here
-
-
-# testing
+def timesTable(n):
+    table = []
+    for i in range(1, n + 1):
+        row = []
+        for j in range(1, n + 1):
+            row.append(i * j)
+        table.append(row)
+    return table
+  
 nums = int(input())
 print(timesTable(nums))
